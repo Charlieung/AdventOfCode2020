c91f922d9b95d1338ffbf790916ea53e421fe5e7
charlieung <leung.charles@outlook.com>
2020-12-18 02:13:43 -0500

    task: day 18 #2

--- Python/18/main.py
+++ Python/18/main.py
@@ -1,4 +1,4 @@
-import os, logging
+import os, logging, numpy as np
 from datetime import datetime as dt
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
@@ -13,7 +13,7 @@ def timeit(f):
         return result
     return wrap
 
-def solve(input):
+def solve_equation_one(input):
     input = input.replace(' ', '')
     
     numbers = []
@@ -36,7 +36,7 @@ def solve(input):
                 if char == ')':
                     parenthesis_count -= 1
             subequation = subequation[1:] # remove initial '('
-            subanswer = solve(subequation)
+            subanswer = solve_equation_one(subequation)
             numbers.append(subanswer)
         else:
             numbers.append(int(char))
@@ -52,19 +52,68 @@ def solve(input):
             raise ValueError(f'Invalid Operation: {operation}')
     return answer
 
+def solve_equation_two(input):
+    input = input.replace(' ', '')
+    
+    numbers = []
+    operations = []
+    
+    i = 0
+    while i < len(input):
+        char = input[i]
+        if char in ['+', '*']:
+            operations.append(char)
+        elif char == '(':
+            parenthesis_count = 1
+            subequation = ''
+            while parenthesis_count != 0:
+                subequation += char
+                i += 1
+                char = input[i]
+                if char == '(':
+                    parenthesis_count += 1
+                if char == ')':
+                    parenthesis_count -= 1
+            subequation = subequation[1:] # remove initial '('
+            subanswer = solve_equation_two(subequation)
+            numbers.append(subanswer)
+            if operations and operations[-1] == '+':
+                number_one = numbers.pop()
+                number_two = numbers.pop()
+                operations.pop()
+                numbers.append(number_one + number_two)
+        else:
+            numbers.append(int(char))
+            if operations and operations[-1] == '+':
+                number_one = numbers.pop()
+                number_two = numbers.pop()
+                operations.pop()
+                numbers.append(number_one + number_two)
+        i += 1
+
+        answer = np.prod(np.array(numbers, dtype=np.int64))
+    return answer
+
+
 @timeit
 def solve_one(input):
     answer = 0
     for line in input.split('\n'):
-        answer += solve(line)
+        answer += solve_equation_one(line)
     logging.info(f'answer = {answer}')
     return answer
 
 @timeit
 def solve_two(input):
-    answer = 0
+    answers = np.array([], dtype=np.int64)
+    for line in input.split('\n'):
+        answers = np.append(answers, solve_equation_two(line))
+    
+    answer = np.sum(answers)
+
     logging.info(f'answer = {answer}')
     return answer
+
 
 if __name__ == '__main__':
     # setup
@@ -77,5 +126,5 @@ if __name__ == '__main__':
     solve_one(input)
 
     # # part 2
-    # logging.info('[Part 2]')
-    # solve_two(expense_report, 2020)
+    logging.info('[Part 2]')
+    solve_two(input)
--- Python/18/test_main.py
+++ Python/18/test_main.py
@@ -1,36 +1,66 @@
 import pytest
-from main import solve#, solve_two
+from main import solve_equation_one, solve_equation_two
 
 # @pytest.fixture
 # def expense_report():
 #     return [1721, 979, 366, 299, 675, 1456]
 
-def test_solve_1():
+def test_solve_one_1():
     input = '1 + 2 * 3 + 4 * 5 + 6'
-    answer = solve(input)
+    answer = solve_equation_one(input)
     assert answer == 71
 
-def test_solve_2():
+def test_solve_one_2():
     input = '1 + (2 * 3) + (4 * (5 + 6))'
-    answer = solve(input)
+    answer = solve_equation_one(input)
     assert answer == 51
 
-def test_solve_3():
+def test_solve_one_3():
     input = '2 * 3 + (4 * 5)'
-    answer = solve(input)
+    answer = solve_equation_one(input)
     assert answer == 26
 
-def test_solve_4():
+def test_solve_one_4():
     input = '5 + (8 * 3 + 9 + 3 * 4 * 3)'
-    answer = solve(input)
+    answer = solve_equation_one(input)
     assert answer == 437
 
-def test_solve_5():
+def test_solve_one_5():
     input = '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))'
-    answer = solve(input)
+    answer = solve_equation_one(input)
     assert answer == 12240
 
-def test_solve_6():
+def test_solve_one_6():
     input = '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'
-    answer = solve(input)
+    answer = solve_equation_one(input)
     assert answer == 13632
+
+def test_solve_two_1():
+    input = '1 + 2 * 3 + 4 * 5 + 6'
+    answer = solve_equation_two(input)
+    assert answer == 231
+
+def test_solve_two_2():
+    input = '1 + (2 * 3) + (4 * (5 + 6))'
+    answer = solve_equation_two(input)
+    assert answer == 51
+
+def test_solve_two_3():
+    input = '2 * 3 + (4 * 5)'
+    answer = solve_equation_two(input)
+    assert answer == 46
+
+def test_solve_two_4():
+    input = '5 + (8 * 3 + 9 + 3 * 4 * 3)'
+    answer = solve_equation_two(input)
+    assert answer == 1445
+
+def test_solve_two_5():
+    input = '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))'
+    answer = solve_equation_two(input)
+    assert answer == 669060
+
+def test_solve_two_6():
+    input = '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'
+    answer = solve_equation_two(input)
+    assert answer == 23340
